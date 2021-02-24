43709d6802cd5bb3e63f3d779f516732eeb23e07
Matheus H. Gois <matheus.hengo@gmail.com>
2021-02-24 18:08:48 -0300

    Terceiro exercicio feito

--- bloco_4/dia_1/index.js
+++ bloco_4/dia_1/index.js
@@ -6,7 +6,8 @@ console.log("Exercício 1: ");
 
 const numA = 348;
 const numB = 578;
-console.log();
+
+
 console.log(`Num A: ${numA} | Num B: ${numB}`);
 console.log(`Adição:      ${numA + numB}`);
 console.log(`Subtração:   ${numA - numB}`);
@@ -20,4 +21,17 @@ if (numA > numB) {
     console.log("NumA é maior que NumB");
 } else {
     console.log("NumA é menor que NumB");
+}
+
+console.log("---------------------")
+console.log("Exercício 3: ");
+
+const numC = 825;
+
+if (numA > numB && numA > numC) {
+    console.log("NumA é o maior");
+} else if (numB > numC && numB > numA) {
+    console.log("NumB é o maior");
+} else {
+    console.log("NumC é o maior");
 }
