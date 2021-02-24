9fab569995dd6a3020114e87925e54ff6940d4fd
Matheus H. Gois <matheus.hengo@gmail.com>
2021-02-24 18:03:58 -0300

    Segundo exercicio feito

--- bloco_4/dia_1/index.js
+++ bloco_4/dia_1/index.js
@@ -1,12 +1,23 @@
-console.log("-----Hello-World-----")
-console.log("-----Aula -- 4.1-----")
+console.log("-----Hello-World-----");
+console.log("-----Aula -- 4.1-----");
+
+console.log();
+console.log("Exercício 1: ");
 
 const numA = 348;
 const numB = 578;
-console.log()
-console.log(`Num A: ${numA} | Num B: ${numB}`)
-console.log(`Adição:      ${numA + numB}`)
-console.log(`Subtração:   ${numA - numB}`)
-console.log(`Multiplic.:  ${numA * numB}`)
-console.log(`Divisão:     ${numA / numB}`)
-console.log(`Resto:       ${numA % numB}`)
+console.log();
+console.log(`Num A: ${numA} | Num B: ${numB}`);
+console.log(`Adição:      ${numA + numB}`);
+console.log(`Subtração:   ${numA - numB}`);
+console.log(`Multiplic.:  ${numA * numB}`);
+console.log(`Divisão:     ${numA / numB}`);
+console.log(`Resto:       ${numA % numB}`);
+
+console.log("---------------------")
+console.log("Exercício 2: ");
+if (numA > numB) {
+    console.log("NumA é maior que NumB");
+} else {
+    console.log("NumA é menor que NumB");
+}
