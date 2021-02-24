1e6e412647365eb92b2b3522f73d880cd91cf159
Matheus H. Gois <matheus.hengo@gmail.com>
2021-02-24 16:41:27 -0300

    Primeiro exercicio feito

---
+++ bloco_4/dia_1/index.js
@@ -0,0 +1,12 @@
+console.log("-----Hello-World-----")
+console.log("-----Aula -- 4.1-----")
+
+const numA = 348;
+const numB = 578;
+console.log()
+console.log(`Num A: ${numA} | Num B: ${numB}`)
+console.log(`Adição:      ${numA + numB}`)
+console.log(`Subtração:   ${numA - numB}`)
+console.log(`Multiplic.:  ${numA * numB}`)
+console.log(`Divisão:     ${numA / numB}`)
+console.log(`Resto:       ${numA % numB}`)
