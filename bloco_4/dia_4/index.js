b82a897a8578b4745fd6f02f7f3f2079db7cc059
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-01 15:37:33 -0300

    4º e 5º exercicios feitos

--- bloco_4/dia_4/index.js
+++ bloco_4/dia_4/index.js
@@ -29,3 +29,30 @@ console.log("Exercicio 3: ");
 for (let infos in info) {
     console.log(infos);
 }
+
+console.log();
+console.log("---------------------");
+console.log("Exercicio 4: ");
+
+for (let infos in info) {
+    console.log(info[infos]);
+}
+
+console.log();
+console.log("---------------------");
+console.log("Exercicio 5: ");
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+}
+
+info2['recorrente'] = 'Sim';
+
+console.log(`${info.personagem} e ${info2.personagem}`);
+console.log(`${info.origem} e ${info2.origem}`);
+console.log(`${info.nota} e ${info2.nota}`);
+if (info.recorrente == "Sim" && info2.recorrente == "Sim") {
+    console.log("Ambos recorrentes");
+}
