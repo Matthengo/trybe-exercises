9cd290bf07396c544897257acafb8bebbe2a0ce3
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-03 20:07:04 -0300

    1º exercicio e exercicios do conteudo parte II e III feitos.

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,26 @@
+/*
+Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+- document.getElementById()
+- document.getElementsByClassName()
+- document.getElementsByTagName()
+1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+3. Crie uma função que mude a cor do quadrado vermelho para branco.
+4. Crie uma função que corrija o texto da tag <h1>.
+5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+*/
+
+/**
+ * Função que muda um texto de um elemento HTML.
+ * @param {string} element      Elemento a ser modificado
+ * @param {string} text         Texto a ser adicionado
+ * @param {number} elementNum   Posição do elemento
+ */
+function changeText(element, text, elementNum) {
+    document.querySelectorAll(element)[(elementNum - 1)].innerText = text;
+}
+
+const text = 'Eu imagino que daqui 2 anos eu esteja trabalhando na área de programação, independente de qual seja e esteja conseguindo realizar meus sonhos.';
+
+changeText('p', text, 2);
