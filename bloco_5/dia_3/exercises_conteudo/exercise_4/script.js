4e242bff86f14d8676a163f0476154179231d562
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-05 18:45:19 -0300

    Exercicio 4 do conteudo arrumado e exercicios iniciados

--- bloco_5/dia_3/exercises_conteudo/exercise_4/script.js
+++ bloco_5/dia_3/exercises_conteudo/exercise_4/script.js
@@ -8,23 +8,28 @@ const caixaTexto = document.querySelector('.caixa-texto');
 
 function changeColor(caixa, color = 'green') {
     caixa.style.backgroundColor = color;
-}
+};
 
 function changeText(caixa) {
     caixa.innerText = caixaTexto.value;
-}
+};
 
 function pasted() {
-    if (caixaTexto === 'Go Trybe!') {
-        alert('Go Trybe!');
-    }
-
-}
+    alert('Go Trybe!');
+};
 
 botaoUm.addEventListener('mouseover', () => {
     changeColor(caixaUm);
-})
-
+});
 botaoDois.addEventListener('mouseover', () => {
     changeColor(caixaDois)
-})
+});
+
+botaoUm.addEventListener('click', () => {
+    changeText(caixaUm)
+});
+botaoDois.addEventListener('click', () => {
+    changeText(caixaDois)
+});
+
+caixaTexto.addEventListener('paste', pasted)
