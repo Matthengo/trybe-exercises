f139fe6f659862c38be8ee2a95cfa78d7dcdbfc5
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-08 20:32:51 -0300

    1º exercicio feito

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -11,4 +11,42 @@ function createDaysOfTheWeek() {
     };
 };
 
+function isSomeDayEqual(day, arrayOfDays) {
+    let isEqual = false;
+    for (let holiday of arrayOfDays) {
+        if (parseInt(holiday) === parseInt(day)) {
+            isEqual = true;
+            break;
+        }
+    }
+
+    return isEqual;
+}
+
+function createDaysOfTheMonth() {
+    const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const holidays = [24, 25, 31];
+    const fridays = [4, 11, 18, 25];
+
+    const dezDaysList = document.querySelector('#days');
+
+    for (let index in dezDays) {
+        const days = dezDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+        dayListItem.classList.add('day');
+
+        if (isSomeDayEqual(days, holidays)) {
+            dayListItem.classList.add('holiday');
+        }
+
+        if (isSomeDayEqual(days, fridays)) {
+            dayListItem.classList.add('friday');
+        }
+
+        dezDaysList.appendChild(dayListItem);
+    }
+}
+
 createDaysOfTheWeek();
+createDaysOfTheMonth();
