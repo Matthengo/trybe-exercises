ec08ad82b00cc1542df5a740cb628f704f866d6c
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-20 20:05:57 -0300

    getUserName() tests (async/await and then/catch) done

--- bloco_10/dia_2/exercises-day2.js
+++ bloco_10/dia_2/exercises-day2.js
@@ -3,5 +3,24 @@ const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
 // ------------------------------------------------------------- 
+// -----Exercise 2---------------------------------------------- 
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
 
-module.exports = { uppercase };
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      };
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+// ------------------------------------------------------------- 
+module.exports = { uppercase, getUserName };
--- bloco_10/dia_2/exercises-day2.test.js
+++ bloco_10/dia_2/exercises-day2.test.js
@@ -1,4 +1,4 @@
-const { uppercase } = require('./exercises-day2.js');
+const { uppercase, getUserName } = require('./exercises-day2.js');
 
 // Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas
 
@@ -8,5 +8,42 @@ describe('uppercase() Function', () => {
       expect(word).toEqual('TEST');
       done();
     })
+  });
+});
+
+// Utilizando a sintaxe de Promise , faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+
+describe('getUserName() Function', () => {
+  it('returns a users name when passed a right ID', () => {
+    expect.assertions(1);
+    return getUserName(4).then((person) => {
+      expect(person).toEqual('Mark');
+    });
+  });
+
+  it('returns an error when passed a wrong ID', () => {
+    expect.assertions(1);
+    return getUserName(1).catch((error) => {
+      expect(error).toStrictEqual({"error": "User with 1 not found."});
+    })
+  });
+});
+
+// Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
+
+describe('getUserName() Function with Async/Await', () => {
+  it('returns a users name when passed a right ID', async () => {
+    expect.assertions(1);
+    const userName = await getUserName(4);
+    expect(userName).toEqual('Mark');
+  });
+
+  it('returns an error when passed a wrong ID', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(1);
+    } catch (error) {
+      expect(error).toEqual({"error": "User with 1 not found."});
+    }
   });
 });
