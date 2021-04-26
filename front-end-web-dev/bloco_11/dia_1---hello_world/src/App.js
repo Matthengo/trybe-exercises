46559bbb90c771b44ed2e8d9a975680290900b93
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-26 20:25:34 -0300

    Exercise list of tasks done

---
+++ front-end-web-dev/bloco_11/dia_1---hello_world/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloMyFriends from './components/HelloMyFriends';
+
+const tasks = ['Bash', 'Git/Github', 'HTML', 'CSS', 'Javascript', 'Jest', 'React'];
+
+const setTask = (task) => {
+  return (
+    <li key={task}> { task } </li>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloMyFriends />
+        <ul>
+          <p>List of Technologies learned: </p>
+          { tasks.map((task) => setTask(task)) }
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
